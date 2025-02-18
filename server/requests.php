d394dfebc68e07082068707f26568700cf4ec228
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-02-18 21:22:29 +0500

    create database connection

--- server/requests.php
+++ server/requests.php
@@ -4,6 +4,9 @@ if(isset($_POST['SignUp'])){
     echo "user Name:".$_POST['username']."<br>";
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+
+    $conn->prepare("Insert into users")
 }
 ?>
