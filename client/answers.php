8cb09e1fbb39227930682213501bf6a1bec076e9
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2025-02-27 17:37:02 +0500

    fetching record from database table answer

--- client/answers.php
+++ client/answers.php
@@ -1,3 +1,19 @@
 <div class="container">
     <h5>Answers:</h5>
+
+    <?php
+    include("./common/db.php");
+    $question_id =
+        $query = "select * from answers where question_id = $qid";
+        $result = $conn->query($query);
+        foreach ($result as $row) {
+            # code...
+            $answer = $row['answer'];
+            echo "<div class='row'>
+                <p>
+                    $answer
+                </p>
+            </div>";
+        }
+    ?>
 </div>
--- client/question-details.php
+++ client/question-details.php
@@ -1,25 +1,25 @@
 <div class="container">
-    <h1 class="heading">
-        Question
-    </h1>
-    <div class="col-8">
-        <?php
-        include("./common/db.php");
+  <h1 class="heading">
+    Question
+  </h1>
+  <div class="col-8">
+    <?php
+    include("./common/db.php");
 
-            $query = "select * from questions where id = $qid";
-            $result = $conn->query($query);
+    $query = "select * from questions where id = $qid";
+    $result = $conn->query($query);
 
-            $row = $result->fetch_assoc();
-            echo "<h5 class='question_title'> Question: ".$row['title']."</h5>
-            <p>".$row['description']."</p>";
+    $row = $result->fetch_assoc();
+    echo "<h5 class='question_title'> Question: " . $row['title'] . "</h5>
+            <p>" . $row['description'] . "</p>";
+    include("./client/answers.php");
 
+    ?>
+    <form action="../server/requests.php" method="post">
+      <input type="hidden" name="question_id" value="<?php echo isset($qid) ? $qid : ''; ?>">
+      <textarea name="answer" class="form-control" placeholder="Your Answer..."></textarea>
+      <button class="btn btn-success my-2" type="submit">Write Your Answer</button>
+    </form>
 
-          ?>
- <form action="../server/requests.php" method="post">
-    <input type="hidden" name="question_id" value="<?php echo isset($qid) ? $qid : ''; ?>">
-    <textarea name="answer" class="form-control" placeholder="Your Answer..."></textarea>
-    <button class="btn btn-success my-2" type="submit">Write Your Answer</button>
-</form>
-
-    </div>
+  </div>
 </div>
